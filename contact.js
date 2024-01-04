8fda2a1642c3ed93e16cd9cf89b3328671b73a6a
albioonhasani <154073712+albioonhasani@users.noreply.github.com>
2024-01-04 22:20:30 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,15 @@
+const logo = document.querySelector('.ahlogo');
+                
+logo.addEventListener('mouseenter', () => {
+    logo.style.backgroundColor = '';
+});
+                
+logo.addEventListener('mouseleave', () => {
+    logo.style.backgroundColor = '';
+ });
+
+
+ function submitForm() {
+          
+    alert('Thank you for reaching out!'); 
+  }
---
+++ signup.js
@@ -0,0 +1,14 @@
+const logo = document.querySelector('.logo');
+                
+logo.addEventListener('mouseenter', () => {
+    logo.style.backgroundColor = '';
+});
+                
+logo.addEventListener('mouseleave', () => {
+    logo.style.backgroundColor = '';
+});
+
+function submitForm() {
+          
+    alert('SignUp succsesful!');
+  }
